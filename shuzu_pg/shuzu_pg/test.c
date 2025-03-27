d873f036e65ac956fffd5ae32fb484f4db3bf55a
huhansanlaila <2704595785@qq.com>
2025-03-27 23:51:12 +0800

    学习

--- shuzu_pg/shuzu_pg/test.c
+++ shuzu_pg/shuzu_pg/test.c
@@ -2,13 +2,13 @@
 #include<stdio.h>
 int main()
 {
-	int arr[10] = { 1,2,3,4,5,6,7,8,9,0 };
-	int i=0;
-	while(i<10)
+	int i = 0;
+	int arr[10] = { 0,1,2,3,4,5,6,7,8,9 };
+	while (i < 10)
 	{
 		printf("%d\n", arr[i]);
 		i = i + 1;
-		
+
 	}
 	return 0;
 }
